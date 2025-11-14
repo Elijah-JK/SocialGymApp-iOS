1f0b46f72bcada2c7d63f279cca409b5523ef0e8
Elijah K <elijah.khamo01@utrgv.edu>
2025-11-14 14:20:16 -0600

    login screen update

---
+++ SocialGymApp/Screens/BasicInfoSignUp.swift
@@ -0,0 +1,18 @@
+//
+//  BasicInfoSignUp.swift
+//  SocialGymApp
+//
+//  Created by Elijah K on 11/14/25.
+//
+
+import SwiftUI
+
+struct BasicInfoSignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasicInfoSignUp()
+}
